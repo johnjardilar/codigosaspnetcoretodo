f6a2cc49715d61718b5b14d0e50c5d27331773b5
john <yango901@gmail.com>
2018-03-10 14:06:00 -0500

    crear repositorio

---
+++ Services/ITodoItemService.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AspNetCoreTodo.Models;
+
+namespace AspNetCoreTodo.Services
+{
+    public interface ITodoItemService
+    {
+        Task<IEnumerable<TodoItem>> GetIncompleteItemsAsync();
+    }
+
+    
+
+    
+}
